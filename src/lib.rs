9bc4c8b60888f1e11a2dedbc5f1217c4ff8245b7
Alexander Sergeev <etc9053@gmail.com>
2023-04-14 21:44:42 +0300

    Change stack internals into using a Vec instead of HashSet

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{collections::BTreeSet, path::PathBuf};
+use std::path::PathBuf;
 
 type Item = (PathBuf, u64);
 pub struct DirWiz {
@@ -7,7 +7,7 @@ pub struct DirWiz {
 
 #[derive(Debug)]
 struct Stack {
-    stack: BTreeSet<PathBuf>,
+    stack: Vec<PathBuf>,
 }
 
 #[derive(Debug)]
@@ -18,13 +18,13 @@ pub struct IntoIter {
 
 impl Stack {
     fn pop(&mut self) -> Option<Item> {
-        let dir = self.stack.pop_last()?;
+        let dir = self.stack.pop()?;
         let mut sum = 0;
         for entry in dir.read_dir().expect("read directory") {
             let entry = entry.unwrap();
             let meta = entry.metadata().unwrap();
             if meta.is_dir() {
-                self.stack.insert(entry.path());
+                self.stack.push(entry.path());
             }
             if meta.is_file() {
                 sum += meta.len();
@@ -34,20 +34,29 @@ impl Stack {
         Some((dir, sum))
     }
 
+    /*
+      Only the last top dir can have a stack, so we only need to copy it to resulting one.
+      Other dirs which came before are never read.
+      Sometimes all dirs are on same level, so we only need to break it one-to-one.
+      Stack should be evenly deep from the beginning. We only need to find a point where path length increases.
+    */
     fn explode(mut self) -> Vec<Self> {
-        let mut res = Vec::new();
-
-        while let Some(e) = self.stack.pop_first() {
-            let mut new_stack: BTreeSet<_> = self
+        assert!(self.stack.len() > 1);
+        let top_len = self.stack.first().unwrap().components().count();
+        let mut res = if top_len != self.stack.last().unwrap().components().count() {
+            // Find a breaking point
+            let pos = self
                 .stack
-                .iter()
-                .filter(|p| p.starts_with(&e))
-                .cloned()
-                .collect();
-            self.stack.retain(|p| !p.starts_with(&e));
-            new_stack.insert(e);
-            res.push(Stack { stack: new_stack });
-        }
+                .binary_search_by(|p| p.components().count().cmp(&(top_len + 1)))
+                .unwrap();
+            println!("Splitting at: {pos}");
+            let stack = self.stack.split_off(pos);
+            vec![Stack { stack }]
+        } else {
+            Vec::with_capacity(self.stack.len())
+        };
+        // Explode the rest one-to-one, because those items are still not traversed
+        res.extend(self.stack.into_iter().map(|p| Stack { stack: vec![p] }));
 
         res
     }
@@ -66,7 +75,7 @@ impl IntoIterator for DirWiz {
     fn into_iter(self) -> IntoIter {
         IntoIter {
             work: vec![Stack {
-                stack: BTreeSet::from([self.path]),
+                stack: Vec::from([self.path]),
             }],
             index: 0,
         }
@@ -77,21 +86,14 @@ impl Iterator for IntoIter {
     type Item = Item;
 
     fn next(&mut self) -> Option<Item> {
-        let index = &mut self.index;
-        while !self.work.is_empty() {
-            if let Some(e) = self.work[*index].pop() {
+        while let Some(dir) = self.get_stack() {
+            if let Some(e) = dir.pop() {
+                if dir.stack.is_empty() {
+                    self.remove_stack();
+                }
                 return Some(e);
-            }
-
-            println!("Removed: {index}");
-            self.work.swap_remove(*index);
-
-            if self.work.len() == *index {
-                *index = 0;
-            }
-            if self.work.len() == 1 && self.work[*index].stack.len() > 2 {
-                let exploded = self.work.remove(*index).explode();
-                let _ = std::mem::replace(&mut self.work, exploded);
+            } else {
+                self.remove_stack();
             }
         }
 
@@ -105,5 +107,33 @@ impl IntoIter {
         if self.index == self.work.len() {
             self.index = 0;
         }
+    }
+
+    fn remove_stack(&mut self) {
+        //println!("Removed: {index}");
+        self.work.swap_remove(self.index);
+
+        if self.work.len() == self.index {
+            self.index = 0;
+        }
+    }
+
+    /// Returns a mutable reference to the current stack or `None` if `work` is empty.
+    /// If there is only one stack left and it has more than one element, it will be exploded into
+    /// smaller stacks to facilitate iteration.
+    fn get_stack(&mut self) -> Option<&mut Stack> {
+        println!("Work [{}]: {:?}", self.work.len(), self.work);
+
+        if self.work.is_empty() {
+            return None;
+        }
+
+        if self.work.len() == 1 && self.work[0].stack.len() > 1 {
+            let exploded = self.work.remove(0).explode();
+            self.work = exploded;
+            self.index = 0;
+        }
+
+        self.work.get_mut(self.index)
     }
 }
